a65f677d5abe5d7eba1f2231f88a7feb9873762a
winnubstj <winnubstj@janelia.hhmi.org>
2021-03-09 15:28:33 -0500

    log counter now start at 1

--- univideo/process_folder.py
+++ univideo/process_folder.py
@@ -11,7 +11,7 @@ def process_folder(base_folder):
     file_list = glob.glob(f'{os.path.join(base_folder,"*.univideo")}')
     print(f'Found {len(file_list)} univideo files in {base_folder}\n')
     for counter,file in enumerate(file_list):
-        print(f'Processing: {os.path.basename(file)} ({counter} of {len(file_list)})')
+        print(f'Processing: {os.path.basename(file)} ({counter+1} of {len(file_list)})')
         process_univideo(file)
 
 if __name__ == "__main__":
